ac75fbe5d47f2786c7b67de2e15838ca69d19d32
pattyzamsal <pazs1991@gmail.com>
2018-08-17 21:59:20 -0500

    Example of colliders and rigidbody

---
+++ Clase 1 Rigoberto/Assets/Scripts/Missile.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Missile : MonoBehaviour {
+
+    public float missileVelocity;
+    private Rigidbody missileBody;
+
+	// Use this for initialization
+	void Awake() {
+        missileBody = this.gameObject.GetComponent<Rigidbody>();
+        missileBody.velocity = Vector3.forward * missileVelocity;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        missileBody.velocity = Vector3.forward * missileVelocity;
+    }
+}
