68d1c05d644a64d4143fdc14fcdde345d83094d6
pattyzamsal <pazs1991@gmail.com>
2018-08-22 21:57:59 -0500

    Manager the character

---
+++ Clases 2D/Clases2d/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+
+	public Camera mainCamera;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+		mainCamera.transform.position = new Vector3(this.transform.position.x, mainCamera.transform.position.y, mainCamera.transform.position.z);
+	}
+}
---
+++ Clases 2D/Clases2d/Assets/Scripts/KirbyController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KirbyController : MonoBehaviour {
+
+	public float velocity;
+	public float jump;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// We detect when the user is pressing
+		if (Input.GetKey(KeyCode.RightArrow)) {
+			this.transform.Translate(Vector2.right * Time.deltaTime * velocity);
+		} else if (Input.GetKey(KeyCode.LeftArrow)) {
+			this.transform.Translate(Vector2.left * Time.deltaTime * velocity);
+		}
+	}
+
+	void FixedUpdate() {
+		if (Input.GetKeyDown(KeyCode.UpArrow)) {
+			this.gameObject.GetComponent<Rigidbody2D>().AddForce(Vector2.up * jump, ForceMode2D.Impulse);
+		}
+	}
+}
