03cf5a1e99c2f7bfe9bfbca4a98191d13dcd3264
Patricia Zambrano <pazs1991@gmail.com>
2018-08-26 23:00:53 -0500

    Create script to load/exit the game

---
+++ Game/Game/Assets/Scripts/IntroScene/MainCanvasScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MainCanvasScript : MonoBehaviour {
+
+	[Tooltip("Pass the play and exit button of the hierarchy")]
+    public Button playBtn, exitBtn;
+
+	void Start () {
+        Button btn1 = playBtn.GetComponent<Button>();
+        Button btn2 = exitBtn.GetComponent<Button>();
+
+        btn1.onClick.AddListener(PlayOnClick);
+        btn2.onClick.AddListener(ExitOnClick);
+	}
+
+    void PlayOnClick() {
+        SceneManager.LoadScene(1);
+    }
+
+    void ExitOnClick() {
+        Application.Quit();
+    }
+}
