4da6bc2d6d9ee7aead673f7506b93ffe89302975
Patricia Zambrano <pazs1991@gmail.com>
2018-09-17 23:23:47 -0500

    Stop animation of walk when the user is dead

--- Game/Game/Assets/Scripts/Game/PlayerController.cs
+++ Game/Game/Assets/Scripts/Game/PlayerController.cs
@@ -55,7 +55,7 @@ public class PlayerController : MonoBehaviour {
                 isJump = true;
             }
         } else {
-
+            isWalk = false;
         }
         activateAnimation();
     }
